b74d98c50cc4143dadab45df96a2e6d4c300955f
tvv <tvv_yar@mail.ru>
2024-01-11 08:59:29 +0300

    1.11 Расстояние между точками

---
+++ src/main/java/ru/condition/Point.java
@@ -0,0 +1,19 @@
+package ru.condition;
+
+public class Point {
+    public static double distance(int x1, int y1, int x2, int y2) {
+        return Math.sqrt((Math.pow((x2-x1),2) + Math.pow((y2-y1),2)));
+    }
+
+    public static void main(String[] args) {
+        double result = Point.distance(0, 0, 2, 0);
+        System.out.println("result (0, 0) to (2, 0) " + result);
+
+        result = Point.distance(6, 1, 3, 1);
+        System.out.println("result (6, 1) to (3, 1) " + result);
+
+        result = Point.distance(4, 13, 10, 25);
+        System.out.println("result (4, 13) to (10, 25) " + result);
+    }
+}
+
