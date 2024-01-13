37cee7cf73cceb5b08b8249bb3e2e38b1450aa8d
tvv <tvv_yar@mail.ru>
2024-01-13 13:44:33 +0300

    2.1 Модульные тесты

---
+++ src/test/java/ru/converter/ConverterTest.java
@@ -0,0 +1,62 @@
+package ru.converter;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+class ConverterTest {
+
+    @Test
+    void whenConvert140RblThen1Dot4Euro() {
+        float in = 140;
+        float expected = 1.4f;
+        float out = Converter.rubleToEuro(in);
+        float eps = 0.0001f;
+        assertThat(out).isEqualTo(expected, withPrecision(eps));
+    }
+
+    @Test
+    void whenConvert100RblThen1Euro() {
+        float in = 100;
+        float expected = 1;
+        float out = Converter.rubleToEuro(in);
+        float eps = 0.0001f;
+        assertThat(out).isEqualTo(expected, withPrecision(eps));
+    }
+
+    @Test
+    void whenConvert300RblThen3Euro() {
+        float in = 300;
+        float expected = 3;
+        float out = Converter.rubleToEuro(in);
+        float eps = 0.0001f;
+        assertThat(out).isEqualTo(expected, withPrecision(eps));
+    }
+
+    @Test
+    void whenConvert200RblThen2dot2222Dlr() {
+        float in = 200;
+        float expected = 2.2222f;
+        float out = Converter.rubleToDollar(in);
+        float eps = 0.0001f;
+        assertThat(out).isEqualTo(expected, withPrecision(eps));
+    }
+
+    @Test
+    void whenConvert180RblThen2Dlr() {
+        float in = 180;
+        float expected = 2;
+        float out = Converter.rubleToDollar(in);
+        float eps = 0.0001f;
+        assertThat(out).isEqualTo(expected, withPrecision(eps));
+    }
+
+    @Test
+    void whenConvert270RblThen3Dlr() {
+        float in = 270;
+        float expected = 3;
+        float out = Converter.rubleToDollar(in);
+        float eps = 0.0001f;
+        assertThat(out).isEqualTo(expected, withPrecision(eps));
+    }
+
+}
