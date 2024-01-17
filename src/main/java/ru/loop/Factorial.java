a0a414538c5c86aedfa0535f16ee87f1ab33527a
tvv <tvv_yar@mail.ru>
2024-01-17 15:52:42 +0300

    4.5 факториал

--- src/main/java/ru/loop/Factorial.java
+++ src/main/java/ru/loop/Factorial.java
@@ -3,10 +3,9 @@ package ru.loop;
 public class Factorial {
     public static int calc(int n) {
         int result = 1;
-
-        if (n < 2) return result;
-
-        for (int i = 1; i <= n; i++) result *= i;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
 
         return result;
     }
