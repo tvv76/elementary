b0c02dcdbe349b2f7b07bc91fb24655128dfd978
tvv <tvv_yar@mail.ru>
2024-01-17 18:26:59 +0300

    4.4 подсчет суммы четных чисел в диапазоне

--- src/main/java/ru/loop/Counter.java
+++ src/main/java/ru/loop/Counter.java
@@ -5,7 +5,9 @@ public class Counter {
     public static int sumByEven(int start, int finish) {
         int rsl = 0;
         for (int i = start; i <= finish; i++) {
-            rsl += i % 2 == 0 ? i : 0;
+            if (i % 2 == 0) {
+                rsl += i;
+            }
         }
         return rsl;
     }
