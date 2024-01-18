2c3a0780369dcf60e6cbb7da535553edc4c06cfc
tvv <tvv_yar@mail.ru>
2024-01-18 16:12:26 +0300

    4.9 простые числа.

---
+++ src/main/java/ru/loop/PrimeNumber.java
@@ -0,0 +1,14 @@
+package ru.loop;
+
+import static ru.loop.CheckPrimeNumber.check;
+
+public class PrimeNumber {
+    public static int calc(int finish) {
+        int count = 0;
+        for (int i = 2; i <= finish; i++) {
+            if (check(i)) count++;
+        }
+        return count;
+    }
+
+}
