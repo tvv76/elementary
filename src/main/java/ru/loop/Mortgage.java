5d48d86156b4eefc5a08c8a131ed7b8c1d9979bd
tvv <tvv_yar@mail.ru>
2024-01-18 16:22:21 +0300

    4.109 ипотека

---
+++ src/main/java/ru/loop/Mortgage.java
@@ -0,0 +1,13 @@
+package ru.loop;
+
+public class Mortgage {
+    public static int year(double amount, int salary, double percent) {
+        int year = 0;
+
+        while (amount > 0) {
+            year++;
+            amount += amount * percent / 100 - salary;
+        }
+        return year;
+    }
+}
