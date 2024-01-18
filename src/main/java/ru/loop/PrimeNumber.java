a095b61d604abb57f6ad5b63d801c28da3d9370f
tvv <tvv_yar@mail.ru>
2024-01-18 21:48:36 +0300

    4.9 простые числа.

--- src/main/java/ru/loop/PrimeNumber.java
+++ src/main/java/ru/loop/PrimeNumber.java
@@ -6,7 +6,9 @@ public class PrimeNumber {
     public static int calc(int finish) {
         int count = 0;
         for (int i = 2; i <= finish; i++) {
-            if (check(i)) count++;
+            if (check(i)) {
+                count++;
+            }
         }
         return count;
     }
