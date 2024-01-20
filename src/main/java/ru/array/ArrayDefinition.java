42d19840f2468dad82a07824703d4dc3632cf3d9
tvv <tvv_yar@mail.ru>
2024-01-20 10:56:59 +0300

    5.2 заполнение массива

--- src/main/java/ru/array/ArrayDefinition.java
+++ src/main/java/ru/array/ArrayDefinition.java
@@ -2,13 +2,15 @@ package ru.array;
 
 public class ArrayDefinition {
     public static void main(String[] args) {
-        short[] ages = new short[10];
-        String[] surnames = new String[100500];
-        float[] prices = new float[40];
+        String[] names = new String[4];
+        names[0] = "Иван Иванов";
+        names[1] = "Сидор Петров";
+        names[2] = "Дмитрий Васильев";
+        names[3] = "Корней Чуковский";
 
-        System.out.println("Размер массива ages: " + ages.length);
-        System.out.println("Размер массива surnames: " + surnames.length);
-        System.out.println("Размер массива prices: " + prices.length);
-
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
