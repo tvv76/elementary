829536dc4959601e17b1130447596b2a8b3e93a8
tvv <tvv_yar@mail.ru>
2024-01-20 11:07:42 +0300

    5.5 массивы и цикл for-each

---
+++ src/main/java/ru/array/ArrayLoop.java
@@ -0,0 +1,15 @@
+package ru.array;
+
+public class ArrayLoop {
+    public static void main(String[] args) {
+        int[] numbers = new int[5];
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = i * 2 + 3;
+        }
+
+        for (int num : numbers) {
+            System.out.println(num);
+        }
+    }
+}
