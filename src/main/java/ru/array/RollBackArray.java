c714300185e2888b316d899003a1761b37d15e1d
tvv <tvv_yar@mail.ru>
2024-01-21 14:29:13 +0300

    5.16 обход массива с последнего элемента

---
+++ src/main/java/ru/array/RollBackArray.java
@@ -0,0 +1,12 @@
+package ru.array;
+
+public class RollBackArray {
+    public static int[] rollback(int[] array) {
+        int[] result = new int[array.length];
+
+        for (int i = 0; i < array.length; i++) {
+            result[i] = array[array.length - 1 - i];
+        }
+        return result;
+    }
+}
