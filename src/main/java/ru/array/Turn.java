317e5405130f105464455ad9a80258821100643b
tvv <tvv_yar@mail.ru>
2024-01-21 11:54:47 +0300

    5.12 перевернуть массив

--- src/main/java/ru/array/Turn.java
+++ src/main/java/ru/array/Turn.java
@@ -2,8 +2,7 @@ package ru.array;
 
 public class Turn {
     public static int[] back(int[] array) {
-        int turns = array.length / 2;
-        for (int i = 0; i < turns; i++) {
+        for (int i = 0; i < array.length / 2; i++) {
             int temp = array[i];
             array[i] = array[array.length - 1 - i];
             array[array.length - 1 - i] = temp;
