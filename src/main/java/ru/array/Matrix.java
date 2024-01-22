2a296c3b01efae951a761b5dbc90783e68f4da5e
tvv <tvv_yar@mail.ru>
2024-01-22 17:00:03 +0300

    5.26 таблица умножения

--- src/main/java/ru/array/Matrix.java
+++ src/main/java/ru/array/Matrix.java
@@ -3,8 +3,8 @@ package ru.array;
 public class Matrix {
     public static int[][] multiple(int size) {
         int[][] table = new int[size][size];
-        for (int row = 0; row < table.length; row++) {
-            for (int col = 0; col < table[row].length; col++) {
+        for (int row = 0; row < size; row++) {
+            for (int col = 0; col < size; col++) {
                 table[row][col] = (row + 1) * (col + 1);
             }
         }
