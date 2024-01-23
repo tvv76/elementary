2e93dd5f9aa410d9aaec10a909ffdb7f24e5d800
tvv <tvv_yar@mail.ru>
2024-01-23 18:06:31 +0300

    5.31 метод двух указателей

--- src/main/java/ru/array/TwoNumberSum.java
+++ src/main/java/ru/array/TwoNumberSum.java
@@ -2,16 +2,17 @@ package ru.array;
 
 public class TwoNumberSum {
     public static int[] getIndexes(int[] array, int target) {
-        for (int i = 0; i < array.length - 1; i++) {
-            for (int j = i + 1; j < array.length; j++) {
-                if (array[i] + array[j] == target) {
-                    return new int[] {i, j};
-                } else if (array[i] + array[j] > target) {
-                    break;
-                }
+        int i = 0;
+        int j = 1;
+        while (i < array.length) {
+            if (j == array.length || array[i] > target || array[i] + array[j] > target) {
+                j = ++i+1;
+            } else if (array[i] + array[j] == target) {
+                return new int[] {i, j};
+            } else {
+                j++;
             }
         }
         return new int[0];
     }
-
 }
