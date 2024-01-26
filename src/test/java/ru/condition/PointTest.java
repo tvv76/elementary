29a01a562f19edb79dfcf662fa5b43f9b4e086ae
tvv <tvv_yar@mail.ru>
2024-01-26 19:45:25 +0300

    7.13 рефакторинг Point

--- src/test/java/ru/condition/PointTest.java
+++ src/test/java/ru/condition/PointTest.java
@@ -8,44 +8,36 @@ class PointTest {
     @Test
     void when00to20then2() {
         double expected = 2;
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
-        double out = Point.distance(x1, y1, x2, y2);
+        Point a = new Point (0, 0);
+        Point b = new Point (2, 0);
+        double out = a.distance(b);
         assertThat(out).isEqualTo(expected, withPrecision(0.01));
     }
 
     @Test
     void when05to23then2dot83() {
         double expected = 2.83;
-        int x1 = 0;
-        int y1 = 5;
-        int x2 = 2;
-        int y2 = 3;
-        double out = Point.distance(x1, y1, x2, y2);
+        Point a = new Point (0, 5);
+        Point b = new Point (2, 3);
+        double out = a.distance(b);
         assertThat(out).isEqualTo(expected, withPrecision(0.01));
     }
 
     @Test
     void when43to81then4dot47() {
         double expected = 4.47;
-        int x1 = 4;
-        int y1 = 3;
-        int x2 = 8;
-        int y2 = 1;
-        double out = Point.distance(x1, y1, x2, y2);
+        Point a = new Point (4, 3);
+        Point b = new Point (8, 1);
+        double out = a.distance(b);
         assertThat(out).isEqualTo(expected, withPrecision(0.01));
     }
 
     @Test
     void when55to71then4dot47() {
         double expected = 4.47;
-        int x1 = 5;
-        int y1 = 5;
-        int x2 = 7;
-        int y2 = 1;
-        double out = Point.distance(x1, y1, x2, y2);
+        Point a = new Point (5, 5);
+        Point b = new Point (7, 1);
+        double out = a.distance(b);
         assertThat(out).isEqualTo(expected, withPrecision(0.01));
     }
 }
