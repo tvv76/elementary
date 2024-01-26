f8670944ceb7f121fe36860da99dd7b45add8fc8
tvv <tvv_yar@mail.ru>
2024-01-26 18:04:15 +0300

    исправления checkstyle

--- src/main/java/ru/array/TwoNumberSum.java
+++ src/main/java/ru/array/TwoNumberSum.java
@@ -3,7 +3,7 @@ package ru.array;
 public class TwoNumberSum {
     public static int[] getIndexes(int[] array, int target) {
         int i = 0;
-        int j = array.length-1;
+        int j = array.length - 1;
         while (i < j) {
             int sum = array[i] + array[j];
             if (sum == target) {
